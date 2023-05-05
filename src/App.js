dbf5e1b2a756f2139592cfc9d37802107ce64b47
Faisal Abrar <erfaisalabrar@gmail.com>
2023-05-05 11:08:13 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import Login from './component/Login';
+import Signup from './component/Signup';
 
 function App() {
+
+  const [currform, Setcurrform]= useState('login');
+
+  // function toggleForm(fr){
+  //   Setcurrform(fr)
+  // }
+
+  let toggleForm = (fr)=>{
+    Setcurrform(fr)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {currform === 'login' ? <Login onForm={toggleForm} /> : <Signup onForm={toggleForm} />}
     </div>
   );
 }
